e3f4150fe0cefef3f35377c07872fb8d3962160f
EkanshM18 <127775286+EkanshM18@users.noreply.github.com>
2023-04-28 12:39:19 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d52a7ba541msh81aa8b5029e9fa0p16e8c4jsn8c928e7944e6",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "7984f28f0ef6fd38c0c3538222db3127"; // enter your key from openweather API
